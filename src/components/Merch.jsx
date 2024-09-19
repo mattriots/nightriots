63942310725146f219a7ed8f817674b45c865d7d
mattriots <mattdepauw@gmail.com>
2024-09-19 11:29:04 -0700

    gap -> 1

--- src/components/Merch.jsx
+++ src/components/Merch.jsx
@@ -9,7 +9,7 @@ const Merch = () => {
       <div name="merch" className="backdrop-brightness-75">
          {/* <h1 className="text-[#ef392c] text-5xl text-center pt-20">Merch</h1> */}
          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 py-20 px-10">
-            <div className="grid grid-cols-2 gap-4 justify-center">
+            <div className="grid grid-cols-2 gap-1 justify-center">
                <a href="https://wearepk.bandcamp.com/album/into-the-roaring">
                   <img src={roaring} alt="Into the Roaring" className="p-2 w-full max-w-sm" />
                </a>
